9c7c8158e95d797a7ef81c9be38c1a31e240f842
44Matt44 <matt123.3a@gmail.com>
2025-01-14 13:33:44 +0100

    Create gameboard.jsx

---
+++ frontend: React App  /gameboard.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./GameBoard.css";
+
+const GameBoard = () => {
+  return (
+    <div>
+      <h1>Welcome to MazeEscape</h1>
+      <div className="game-board">
+        {/* Maze Grid Placeholder */}
+      </div>
+    </div>
+  );
+};
+
+export default GameBoard;
